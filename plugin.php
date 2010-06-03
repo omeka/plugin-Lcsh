3b1ffb38aa24714f310c1b4626b0d483d661e54c
Jim Safley <jimsafley@gmail.com>
2010-06-03 23:32:05 +0000

    Fixes JavaScript to observe custom event.

--- plugin.php
+++ plugin.php
@@ -1,46 +1,46 @@
 <?php
+add_plugin_hook('admin_theme_header', 'LcshPlugin::adminThemeHeader');
 add_filter(array('Form', 'Item', 'Dublin Core', 'Subject'), 'LcshPlugin::filterDcSubject');
 
 class LcshPlugin
 {
+    public static function adminThemeHeader()
+    {
+?>
+<script type="text/javascript" charset="utf-8">
+document.observe('omeka:elementformload', function(e) {
+    $$('#element-49 input[type="text"]').each(function(input) {
+        Event.observe(input, 'keyup', function(event){
+            if (3 > input.getValue().length) return;
+            new Ajax.Request('<?php echo ADMIN_BASE_URL . '/lcsh/index/lcsh-proxy/'; ?>', {
+                method: 'get',
+                parameters: {'q': input.getValue()},
+                onSuccess: function(transport, json) {
+                    if (!$(input.id + '-suggest')) {
+                        var suggestDiv = new Element('div', {id: input.id + '-suggest'});
+                        input.insert({after: suggestDiv});
+                    }
+                    var html = '<ul>';
+                    for(var i = 0; i < json[1].length; i++) {
+                        html += '<li onclick="$(\'' + input.id + '\').setValue(\'' + json[1][i] + '\')">' + json[1][i] + '</li>';
+                    }
+                    html += '</ul>';
+                    $(input.id + '-suggest').update(html);
+                },
+                onFailure: function() {
+                    alert('failure');
+                }
+            })
+        })
+    })
+})
+</script>
+<?php
+    }
+    
     public static function filterDcSubject($html, $inputNameStem, $value, 
                                            $options, $record, $element)
     {
-        $id = trim($inputNameStem, ']');
-        $id = str_replace('][', '-', $id);
-        $id = str_replace('[', '-', $id);
-        $id = $id . '-text';
-        
-        $lcshProxyUrl = ADMIN_BASE_URL . '/lcsh/index/lcsh-proxy/';
-        
-        // Any HTML other than a form element will be removed when you invoke 
-        // "Add Input." This means the below <script> and <div> tags will be 
-        // removed
-        $js = "
-<script type=\"text/javascript\" charset=\"utf-8\">
-Event.observe('$id', 'keyup', function(event){
-    if (3 > $('$id').getValue().length) return;
-    new Ajax.Request('$lcshProxyUrl', {
-        method: 'get',
-        parameters: {'q': $('$id').getValue()},
-        onSuccess: function(transport, json) {
-            //alert(json ? Object.inspect(json) : 'no JSON object');
-            var html = '<ul>'; 
-            for(var i = 0; i < json[1].length; i++) {
-                html += '<li onclick=\"$(\'$id\').setValue(\'' + json[1][i] + '\')\">' + json[1][i] + '</li>';
-            }
-            html += '</ul>';
-            $('$id-suggest').update(html)
-        },
-        onFailure: function() {
-            alert('failure');
-        }
-    })
-});
-</script>";
-//exit($js);
-        return __v()->formText($inputNameStem . '[text]', $value, array('size' => '50')) 
-               . $js 
-               . '<div id="' . $id . '-suggest"></div>' ;
+        return __v()->formText($inputNameStem . '[text]', $value, array('size' => '50'));
     }
 }
